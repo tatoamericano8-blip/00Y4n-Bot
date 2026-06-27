4d12a5c5f9a98229b11213d3c71ad7730b754ad6
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-06-26 23:16:50 -0300

    Update fastpass.js

--- src/commands/Tools/fastpass.js
+++ src/commands/Tools/fastpass.js
@@ -53,7 +53,7 @@ export default {
 
         // Embed con estética 00Y4n
         const embedFastPass = new EmbedBuilder()
-            .setTitle(`${EMOJIS.coraaMov} FastPass de la Sesión ${EMOJIS.coraaMov}`)
+            .setTitle(`${EMOJIS.coraaMov} __FastPass de la Sesión__ ${EMOJIS.coraaMov}`)
             .setDescription(`${EMOJIS.flechaV} El FastPass ha sido **liberado para la sesión**. Los miembros que adquirieron su pase de FastPass y el Equipo de Staff ya pueden unirse utilizando el botón de abajo.\n\n*Compartir este enlace resultará en la revocación permanente de tus permisos de FastPass.*\n\n${EMOJIS.flechaH} ¿Quieres unirte antes que el resto? Adquiere tu pase de **FastPass** correspondiente en el canal de beneficios del servidor.`)
             .setColor('#ff6600'); // Tu naranja insignia
 
