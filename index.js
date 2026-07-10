0da31d155b1dc52193c54586fb9da25c674cadbb
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-07-10 20:23:51 -0300

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import './src/app.js';
-const app = express();
-app.get('/', (req, res) => res.send('¡Bot de SWFL Online 24/7 en Render!'));
-app.listen(process.env.PORT || 3000, () => console.log('Servidor listo.'));
