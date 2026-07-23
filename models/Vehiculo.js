ace7de1a91ef7dc995bc0d7b8e2a395d4d9ad817
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-07-23 17:17:01 -0300

    Create Vehiculo.js

---
+++ models/Vehiculo.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const vehiculoSchema = new mongoose.Schema({
+    usuario_id: { type: String, required: true },
+    marca: { type: String, required: true },
+    modelo: { type: String, required: true },
+    ano: { type: String, required: true },
+    color: { type: String, required: true },
+    patente: { type: String, required: true, unique: true }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('Vehiculo', vehiculoSchema);
