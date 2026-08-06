05d07d5a5881ce5457ac44e3c778b62328f3f028
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-08-06 11:13:02 -0300

    RESTORE app.js + import parcheFooterEmbed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,51 @@
-PLACEHOLDER_WILL_FIX
+import 'dotenv/config';
+import './utils/parcheColorEmbed.js';
+import './utils/parcheFooterEmbed.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import express from 'express';
+import cron from 'node-cron';
+import mongoose from 'mongoose';
+
+import config from './config/application.js';
+import { initializeDatabase } from './utils/database.js';
+import { getGuildConfig } from './services/guildConfig.js';
+import { getServerCounters, saveServerCounters, updateCounter } from './services/serverstatsService.js';
+import { logger, startupLog, shutdownLog } from './utils/logger.js';
+import { checkBirthdays } from './services/birthdayService.js';
+import { checkGiveaways } from './services/giveawayService.js';
+import { loadCommands, registerCommands as registerSlashCommands } from './handlers/commandLoader.js';
+
+import { lanzarOportunidadEconomica } from './utils/gestorOportunidades.js';
+import { procesarMensajeRecordatorio } from './utils/gestorRecordatorios.js';
+import { procesarCiudadanoDelDia } from './utils/ciudadanoDelDia.js';
+
+class TitanBot extends Client {
+  constructor() {
+    const intents = [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildVoiceStates,
+      GatewayIntentBits.GuildModeration,
+      GatewayIntentBits.GuildPresences,
+    ].filter((x) => typeof x === 'number');
+
+    super({
+      intents,
+      rest: { timeout: 20000 },
+      failIfNotExists: false,
+    });
+
+    this.config = config;
+    this.commands = new Collection();
+    this.events = new Collection();
+    this.buttons = new Collection();
+    this.selectMenus = new Collection();
+    this.modals = new Collection();
+    this.cooldowns = new Collection();
+    this.db = null;
+    this.rest = new REST({ version: '10' }).setToken(config.bot.token);
+  }
