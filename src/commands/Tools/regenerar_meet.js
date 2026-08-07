4661aef47c5a73d791595b83c2581d5d8fbc7c17
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-08-07 18:37:47 -0300

    Update regenerar_meet.js

--- src/commands/Tools/regenerar_meet.js
+++ src/commands/Tools/regenerar_meet.js
@@ -70,13 +70,13 @@ export default {
         }
 
         // --- ENVIAR NUEVO ANUNCIO DE REGENERACIÓN ---
-        const textoDescripcion = `<a:mov:1523027371735777503> <@${usuarioStaff.id}> ha **regenerado el link del Car Meet (x${contador})**! Por favor, sean pacientes ya que las próximas reinvitaciones serán dentro de 10-15 minutos. Molestar al host para pedir el acceso resultará en un aislamiento (timeout).`;
+        const textoDescripcion = `<:dot:1534938142665084938> <@${usuarioStaff.id}> ha **regenerado el link del Car Meet (x${contador})**! Por favor, sean pacientes ya que las próximas reinvitaciones serán dentro de 10-15 minutos. Molestar al host para pedir el acceso resultará en un aislamiento (timeout).`;
         
         // Imagen por defecto si no se proporciona una
-        const urlImagenPredeterminada = 'https://cdn.discordapp.com/attachments/1517331229303902432/1525722876685324368/Link_Regenerado_NUEVO23.png?ex=6a546ba8&is=6a531a28&hm=0d5c2f80951f5909ed7ffede7308abdbe6f9eac502552f1dfc70da38bf3c4117';
+        const urlImagenPredeterminada = 'https://cdn.discordapp.com/attachments/1452644461745148049/1535401345916473394/Link_regenerado_1.png?ex=6a77a16f&is=6a764fef&hm=d42d4aff4f2e5e2f8e5b8ef9c7162a6b8b6338937975cbe06cada5e1d411ff05';
 
         const embedRegenMeet = new EmbedBuilder()
-            .setTitle('<a:espe:1523026421512142899> SWFL Car Meet | Link Regenerado <a:espe:1523026421512142899>')
+            .setTitle('<a:espe:1534954231138746488> SWFL Car Meet | *Link Regenerado* <a:espe:1534954231138746488>')
             .setDescription(textoDescripcion)
             .setColor('#74d4fc')
             .setImage(fotoAdjunta ? fotoAdjunta.url : urlImagenPredeterminada);
