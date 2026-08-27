d8ebbc0144e7d5720e695fceaa2a0a4f1b2828e5
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-08-27 13:43:39 -0300

    Update mensajeReacciones.js

--- src/events/mensajeReacciones.js
+++ src/events/mensajeReacciones.js
@@ -63,7 +63,7 @@ export default {
                 const nombreSesion = startup.tipo === 'rp' ? 'Roleplay' : 'Car Meet';
 
                 const embedSetup = new EmbedBuilder()
-                    .setTitle(`<a:mariquieta:1534954231138746488> Southwest Florida Comunidad 00Y4n — Configuración del Servidor <a:mariquieta:1534954231138746488>`)
+                    .setTitle(`<a:mariquieta:1534954231138746488> Southwest Florida Comunidad 00Y4n — __*Configuración del Servidor*__ <a:mariquieta:1534954231138746488>`)
                     .setDescription(
                         `<:dot:1534938142665084938> <@${startup.hostId}> **ha comenzado a configurar su servidor** para la sesión de **${nombreSesion}!** Los miembros con acceso anticipado podrán unirse en breve usando el enlace de early access. ¡Asegurate de boostear el servidor para early access!\n\n` +
                         `<:replican:1542264548801777685> __Por favor sé paciente y dale al host un tiempo razonable para configurar__.`
