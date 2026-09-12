893497d0e23bd619cfef5abed9a51907bbc15df0
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-09-12 12:53:26 -0300

    Update bot presence activity name

--- src/config/bot.js
+++ src/config/bot.js
@@ -7,7 +7,7 @@ export const botConfig = {
     status: "online",
     activities: [
       {
-        name: "🚧00Y4n🚧",
+        name: "💎 #1 Roleplay hispano 00Y4n",
         type: 0,
       },
     ],
