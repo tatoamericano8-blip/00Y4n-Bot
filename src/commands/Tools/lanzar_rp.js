339fb87727dc4e9c818b4dc272d494e3c3b6e515
tatoamericano8-blip <tatoamericano8@gmail.com>
2026-09-12 11:21:46 -0300

    Change button emoji in lanzar_rp.js

--- src/commands/Tools/lanzar_rp.js
+++ src/commands/Tools/lanzar_rp.js
@@ -128,7 +128,7 @@ export default {
                 .setCustomId('verificar_voto_swfl')
                 .setLabel('Link de la Sesion')
                 .setStyle(ButtonStyle.Secondary)
-                .setEmoji(EMOJI_DEF.lock.id)
+                .setEmoji(EMOJI_DEF.hyperlink.id)
         );
 
         await interaction.reply({ content: 'Liberando accesos de Roleplay...', ephemeral: true });
